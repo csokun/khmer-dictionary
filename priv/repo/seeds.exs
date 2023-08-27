f19ccae0065eb3902b154da05786fe887e65b2f0
Sokun Chorn (The Blockchain Guy) <chornsokun@gmail.com>
2023-08-27 18:10:04 +1000

    import entries from zip

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,26 +9,57 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
-with {:ok, body} <- File.read("priv/db.json"),
-     {:ok, records} <- Jason.decode(body) do
-  records
-  |> Enum.each(fn %{
-                    "main" => main,
-                    "subscript" => subscript,
-                    "subword" => subword,
-                    "pronunciation" => pronunciation,
-                    "part_of_speech" => part_of_speech,
-                    "example" => example,
-                    "definition" => definition
-                  } ->
-    Kd.Repo.insert!(%Kd.Word{
-      main: main,
-      subscript: subscript,
-      subword: subword,
-      pronunciation: pronunciation,
-      part_of_speech: part_of_speech,
-      example: example,
-      definition: definition
-    })
-  end)
+defmodule SeedHelper do
+  def import do
+    case read_json_from_zip("priv/db.zip", "db.json") do
+      {:ok, json} ->
+        import_entries(json)
+
+      {:error, reason} ->
+        IO.puts("error #{reason}")
+    end
+  end
+
+  defp read_json_from_zip(zip_file, json_filename) do
+    with {:ok, file} = File.read(zip_file),
+         {:ok, zip_ref} = :zip.zip_open(file, [:memory]),
+         {:ok, {_, content}} = :zip.zip_get(json_filename, zip_ref),
+         :zip.zip_close(zip_ref) do
+      result =
+        case Jason.decode(content) do
+          {:ok, parsed_json} ->
+            {:ok, parsed_json}
+
+          {:error, reason} ->
+            {:error, "Failed to parse JSON: #{reason}"}
+        end
+
+      result
+    end
+  end
+
+  defp import_entries(entries) do
+    entries
+    |> Enum.each(fn %{
+                      "main" => main,
+                      "subscript" => subscript,
+                      "subword" => subword,
+                      "pronunciation" => pronunciation,
+                      "part_of_speech" => part_of_speech,
+                      "example" => example,
+                      "definition" => definition
+                    } ->
+      Kd.Repo.insert!(%Kd.Word{
+        main: main,
+        subscript: subscript,
+        subword: subword,
+        pronunciation: pronunciation,
+        part_of_speech: part_of_speech,
+        example: example,
+        definition: definition
+      })
+    end)
+  end
 end
+
+SeedHelper.import()
