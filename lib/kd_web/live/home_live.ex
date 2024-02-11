e0dd00f7b1e7962b815530a7d66770177f75500c
Sokun Chorn (The Blockchain Guy) <chornsokun@gmail.com>
2024-02-11 17:31:00 +1100

    simplify search with push_patch

--- lib/kd_web/live/home_live.ex
+++ lib/kd_web/live/home_live.ex
@@ -11,11 +11,28 @@ defmodule KdWeb.HomeLive do
      |> assign(:entries, AsyncResult.loading())}
   end
 
-  def handle_event("search", %{"q" => q}, socket) do
+  def handle_params(params, _url, socket) do
+    q = Map.get(params, "q", "")
+
     {:noreply,
      socket
-     |> assign_async(:entries, fn ->
-       {:ok, %{entries: Kd.Dictionary.lookup("#{q}%")}}
-     end)}
+     |> assign(:query, q)
+     |> search(q)}
+  end
+
+  def handle_event("search", %{"q" => q}, socket) do
+    {:noreply, socket |> push_patch(to: ~p"/?q=#{q}")}
+  end
+
+  defp search(socket, "") do
+    socket
+    |> assign_async(:entries, fn -> {:ok, %{entries: []}} end)
+  end
+
+  defp search(socket, q) do
+    socket
+    |> assign_async(:entries, fn ->
+      {:ok, %{entries: Kd.Dictionary.lookup("#{q}%")}}
+    end)
   end
 end
